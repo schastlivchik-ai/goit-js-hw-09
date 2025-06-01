5e59f69e7655f847d455a2fe5b1fca75222d222f
Avramenko Yehor <avramenko.ego98@gmail.com>
2025-06-01 16:20:42 +0200

    Add feedback form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
-<script type="module" src="./js/2-form.js"></script>
+let formData = {
+    email: "",
+    message: "",
+};
+
+const form = document.querySelector(`.feedback-form`);
+form.addEventListener(`input`, event => {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(`feedback-form-state`, JSON.stringify(formData));
+});
+
+const allInputs = form.querySelectorAll(`input, textarea`)
+form.addEventListener(`submit`, event => {
+    event.preventDefault();
+    if (Array.from(allInputs).some(input => input.value.trim() === ``)) {
+        alert('Fill please all fields');
+        return
+    }
+    console.log(formData);
+    formData = {
+        email: "",
+        message: "",
+    }
+    localStorage.clear();
+    form.reset()
+})
+
+window.addEventListener(`DOMContentLoaded`, () => {
+    if (localStorage.getItem(`feedback-form-state`)) {
+        
+        const savedFormData = JSON.parse(localStorage.getItem(`feedback-form-state`));
+        formData = savedFormData;
+        Object.entries(savedFormData).map((input) => {
+            form.querySelector(`[name="${input[0]}"]`).value = input[1]
+        });
+    };
+});
